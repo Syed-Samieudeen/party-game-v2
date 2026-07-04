c6c5a18db6ffb0f0b1acf5291faf0f3c4112e021
Syed-Samieudeen <syed.samie07@gmail.com>
2026-07-04 15:03:53 +0000

    fix api and socket env

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = 'https://party-game-v2.onrender.com'
+const SOCKET_URL = import.meta.env.VITE_API_URL
 
 export const socket = io(SOCKET_URL)
