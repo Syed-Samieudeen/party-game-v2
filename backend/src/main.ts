14e39b3b457c3bffbde4a6c89c460c597b02a386
Syed-Samieudeen <syed.samie07@gmail.com>
2026-07-04 15:02:24 +0000

    fix cors for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,11 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://party-game-v2.vercel.app'
-    ],
-    credentials: true,
-  })
-
-  await app.listen(process.env.PORT || 3000)
+app.enableCors({
+  origin: true,
+  credentials: true,
+})
+  
+await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
