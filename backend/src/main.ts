5777007185ab3863d7ec36c82656bc4ba93ce5f6
Syed-Samieudeen <syed.samie07@gmail.com>
2026-07-04 14:41:11 +0000

    fix cors production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: [
+      'http://localhost:5173',
+      'https://party-game-v2.vercel.app'
+    ],
     credentials: true,
-  });
+  })
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000)
 }
-bootstrap();
+bootstrap()
